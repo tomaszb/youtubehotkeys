8da37496bb569c11dee644b3a2f9ef347dba424f
tomaszb <tbacewicz@gmail.com>
2013-12-05 00:35:16 -0800

    fix back button bug, add title of song to panel with no functionality yet

--- data/get-buttons.js
+++ data/get-buttons.js
@@ -1,15 +1,17 @@
+//Bind Youtube Playlist page to Addon
 var bind = document.getElementById("bind");
 bind.addEventListener('click', function click(event) {
 	self.port.emit('bind_now');
 }, false);
  
-
+//Goes to next song on the Youtube Playlist
 var next = document.getElementById("next");
 next.addEventListener('click', function click(event){
 	self.port.emit('next');
 }, false);
 
+//Goes to previous song on the Youtube Playlist
 var last = document.getElementById("last");
-next.addEventListener('click', function click(event){
+last.addEventListener('click', function click(event){
 	self.port.emit('last');
 }, false);
